7fe79d450ae60bef617af5c91b2dbdcc23f354af
David Anderson <davidanderson@live.ie>
2024-03-15 14:23:47 +0000

    Test key

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,17 @@
-k_Q6vnu0Sh8pcbjV9PYAhE069zg
+const API_KEY = "k_Q6vnu0Sh8pcbjV9PYAhE069zg";
+const API_URL = "https://ci-jshint.herokuapp.com/api"
+const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
+
+document.getElementById("status").addEventListener("click", e => getStatus(e));
+
+async function getStatus(e) {
+    const queryString = `${API_URL}?api_key=${API_KEY}`;
+
+    const response = await fetch(queryString);
+
+    const data = await response.json();
+
+    if (response.ok) {
+        console.log(data.expiry)
+    }
+}
